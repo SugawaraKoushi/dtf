228ed6bc52b3cb882e80ad6c52bafe204bd5b8d3
Vladislav Fokin <senior.vlados97@gmail.com>
2024-08-09 13:41:05 +0300

    refactor pages

--- About.php
+++ About.php
@@ -26,8 +26,132 @@ if ($path && strpos($path, '..') === false) {
 ?>
 
 <?php
-require 'Menu.php';
-require 'AboutContent.php'; ?>
+require 'Menu.php';?>
+<div class="content-item">
+    <div class="content-header">
+        <div class="author">
+            <div class="image author-icon">
+                <img
+                    class="icon"
+                    src="icons/side-menu/topics/games-icon.webp" />
+            </div>
+            <a class="author-name" href="/popular.html">DTF</a>
+            <div class="author-details">
+                <span class="posting-time">15.05.2024</span>
+            </div>
+        </div>
+    </div>
+    <div class="content-body">
+        <h3 class="content-title">Что такое DTF?</h3>
+        <article class="content-block">
+            <div class="content-text">
+                DTF — это проект, разработанный в процессе
+                изучения дисциплины Web-Технологии. по ходу
+                изучения данной дисциплины предполагается
+                выполнить 4 лабораторные работы:
+            </div>
+            <table class="content-table">
+                <caption class="content-table-caption">
+                    <h3>Лаб. работы</h3>
+                </caption>
+                <thead>
+                    <tr>
+                        <td>№</td>
+                        <td>Цель работы</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="content-column-num">
+                            1.
+                        </td>
+                        <td>
+                            Ознакомиться с понятием вёрстки
+                            веб-страниц, видами вёрстки,
+                            основными тегами и атрибутами
+                            языка HTML, структурой
+                            документа, стилями CSS, их
+                            синтаксисом, способами
+                            подключения, получить
+                            практические навыки создания
+                            листов стилей и веб-страниц.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="content-column-num">
+                            2.
+                        </td>
+                        <td>
+                            Ознакомиться с основными
+                            возможностями языка JavaScript,
+                            синтаксисом, встроенными
+                            объектами, событиями DHTML,
+                            получить практические навыки
+                            программирования на языке
+                            JavaScript. Получить
+                            практические навыки создания
+                            интерактивных веб-страниц.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="content-column-num">
+                            3.
+                        </td>
+                        <td>
+                            Ознакомиться с основными
+                            возможностями языка PHP,
+                            синтаксисом, основными
+                            операциями, получить
+                            практические навыки
+                            программирования.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="content-column-num">
+                            4.
+                        </td>
+                        <td>
+                            Ознакомиться с основными
+                            функциями PHP, применяемыми для
+                            работы с MySQL-сервером, изучить
+                            и применить на практике механизм
+                            сессий.
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </article>
+        <div class="content-counters">
+            <div class="content-counter">14K показов</div>
+            <div class="content-counter">13K открытий</div>
+        </div>
+    </div>
+    <div class="content-footer">
+        <button class="like-button">
+            <img
+                class="like-icon"
+                src="icons/content/like-icon.svg" />
+            <span>320</span>
+        </button>
+        <button class="comments-button">
+            <img
+                class="like-icon"
+                src="icons/content/comment-icon.svg" />
+            <span>86</span>
+        </button>
+        <button class="favourite-button">
+            <img
+                class="like-icon"
+                src="icons/content/favourite-icon.svg" />
+            <span>10</span>
+        </button>
+        <button class="share-button">
+            <img
+                class="like-icon"
+                src="icons/content/share-icon.svg" />
+        </button>
+    </div>
+</div>
 <div class="content-item">
     <div class="content-header">
         <div class="author">
--- Common.php
+++ Common.php
@@ -3,8 +3,27 @@ $title = 'Основные';
 require 'head.php';
 require 'LoggedInHeader.php'; ?>
 
-<?php
-require 'Menu.php';
-require 'CommonContent.php';
-require 'Footer.php';
-?>
+<?php require 'Menu.php'; ?>
+<div class="content-item">
+    <div class="content-header settings-header">
+        <h3>Основные</h3>
+    </div>
+    <div class="content-body">
+        <ul class="settings">
+            <form action="MailHandler.php" method="POST">
+                <li class="form-section">
+                    <h4 class="setting-name">Почта</h4>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        class="field email"
+                        placeholder="example@email.com"
+                        required/>
+                    <button type="submit" class="button-size-m">Сохранить</button>
+                </li>
+            </form>
+        </ul>
+    </div>
+</div>
+<?php require 'Footer.php';?>
